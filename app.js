4dbf7a5652005bd0559544cbed1c7a6e160e7a97
KevinAntonietti <108470673+KevinAntonietti@users.noreply.github.com>
2022-11-24 11:20:07 +0100

    Route express et EJS en place

---
+++ app.js
@@ -0,0 +1,22 @@
+// Require dependencies
+const express = require('express');
+const router = require('./app/routers/router');
+
+// Create express app
+const app = express();
+
+const port = 3002;
+
+// view engine
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+// MW
+
+// Router
+app.use(router);
+
+// Server
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+    });
