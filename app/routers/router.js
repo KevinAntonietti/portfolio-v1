ca7c1a380113ea71bcb50889efd7473d95518916
KevinAntonietti <108470673+KevinAntonietti@users.noreply.github.com>
2022-12-29 09:21:30 +0100

    Data loop for project

--- app/routers/router.js
+++ app/routers/router.js
@@ -2,12 +2,29 @@ const express = require('express');
 
 const router = express.Router();
 
+const projets = require ("../../src/data/projets.json")
+
 router.get('/', function (req,res) {
     res.render('home')
 });
 
 router.get('/projets', function (req,res) {
+    res.locals = {
+        ...res.locals,
+        projets,
+        title: 'Kevin Antonietti',
+        description: 'Mes projets',
+        url: '/projets',
+      };
     res.render('projectPage')
+});
+
+router.get(function (req,res) {
+    res.locals = {
+        ...res.locals,
+        projets,
+      };
+    res.render('projectOverview')
 });
 
 module.exports = router;
