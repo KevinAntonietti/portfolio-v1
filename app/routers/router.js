4a161bb9ea4a456914e2267fc48c66b10dbff21b
KevinAntonietti <108470673+KevinAntonietti@users.noreply.github.com>
2022-12-29 14:01:18 +0100

    add custom route to project

--- app/routers/router.js
+++ app/routers/router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const projets = require ("../../src/data/projets.json")
 
+// Route to the home
 router.get('/', function (req,res) {
   res.locals = {
     ...res.locals,
@@ -15,6 +16,7 @@ router.get('/', function (req,res) {
     res.render('home')
 });
 
+// Route to the project page
 router.get('/projets', function (req,res) {
     res.locals = {
         ...res.locals,
@@ -26,6 +28,7 @@ router.get('/projets', function (req,res) {
     res.render('projectPage')
 });
 
+// Route to the project overview area
 router.get(function (req,res) {
     res.locals = {
         ...res.locals,
@@ -33,5 +36,22 @@ router.get(function (req,res) {
       };
     res.render('projectOverview')
 });
+
+// Route to the various projects
+router.get('/projets/:slug', function (req,res) {
+
+  const item = projets.find(
+    (elem) => elem.slug,
+  );
+  res.locals = {
+      ...res.locals,
+      item,
+      title: `${item.name}`,
+      description: 'Mes projets',
+      url: `/projets/${item.slug}`,
+    };
+  res.render('oneProjectPage')
+});
+
 
 module.exports = router;
