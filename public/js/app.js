35a232bee8d92eea07f85094f6a744461a28a1c1
KevinAntonietti <108470673+KevinAntonietti@users.noreply.github.com>
2023-01-04 10:11:58 +0100

    lightMode Local Stoarage

--- public/js/app.js
+++ public/js/app.js
@@ -1,62 +1,72 @@
 let isLight = false;
+
 const app = {
+  btn: document.getElementById("btn-mode"),
+
+  // Navside element
+  navside: document.querySelectorAll(".navSide__picto__svg"),
+
+  //Header element
+  headerLogo: document.querySelector(".header__logo__svg"),
+
+  // projet element
+  projectOverview: document.querySelectorAll(
+    ".projectOverview__container__next__link__svg"
+  ),
+
+  //oneProjectPage element
+  projectPageWebsite: document.querySelectorAll(
+    ".projectHeader__link__website__svg"
+  ),
+  projectPageGitHub: document.querySelectorAll(
+    ".projectHeader__link__github__svg"
+  ),
+  projectPageBack: document.querySelectorAll(".inconBack__svg"),
+
   init: () => {
+    const savedIsLight = localStorage.getItem('isLight');
+    if (savedIsLight !== null) {
+      isLight = savedIsLight === 'true';
+      app.setLightMode(isLight);
+    }
     app.toggleColorMod();
   },
 
   toggleColorMod: () => {
-    // Body element
-    const btn = document.getElementById("btn-mode");
-
-    // Navside element
-    const navside = document.querySelectorAll(".navSide__picto__svg");
-
-    //Header element
-    const headerLogo = document.querySelector(".header__logo__svg");
-
-    // projet element
-    const projectOverview = document.querySelectorAll(
-      ".projectOverview__container__next__link__svg"
-    );
-
-    //oneProjectPage element
-    const projectPageWebsite = document.querySelectorAll(
-      ".projectHeader__link__website__svg"
-    );
-    const projectPageGitHub = document.querySelectorAll(
-      ".projectHeader__link__github__svg"
-    );
-    const projectPageBack = document.querySelectorAll(".inconBack__svg");
-
-    btn.addEventListener("click", () => {
+    app.btn.addEventListener("click", () => {
       isLight = !isLight;
-      // CHanger dans le body
-      document.body.classList.toggle("light-mode-body");
+      app.setLightMode(isLight);
+      localStorage.setItem('isLight', isLight);
+    });
+  },
 
-      //changer dans la navside
-      navside.forEach((item) => {
-        item.classList.toggle("light-mode-svg");
-      });
+  setLightMode: (isLight) => {
+    // CHanger dans le body
+    document.body.classList.toggle("light-mode-body",isLight);
 
-      //changer dans le header
-      headerLogo.classList.toggle("light-mode-svg");
+    //changer dans la navside
+    app.navside.forEach((item) => {
+      item.classList.toggle("light-mode-svg",isLight);
+    });
 
-      // Changer dans la page projetOverview
-      projectOverview.forEach((item) => {
-        item.classList.toggle("light-mode-svg");
-      });
+    //changer dans le header
+    app.headerLogo.classList.toggle("light-mode-svg",isLight);
 
-      // CHanger dans la page projet
-      projectPageWebsite.forEach((item) => {
-        item.classList.toggle("light-mode-svg");
-      });
-      projectPageGitHub.forEach((item) => {
-        item.classList.toggle("light-mode-svg");
-      });
+    // Changer dans la page projetOverview
+    app.projectOverview.forEach((item) => {
+      item.classList.toggle("light-mode-svg",isLight);
+    });
 
-      projectPageBack.forEach((item) => {
-        item.classList.toggle("light-mode-svg");
-      });
+    // CHanger dans la page projet
+    app.projectPageWebsite.forEach((item) => {
+      item.classList.toggle("light-mode-svg",isLight);
+    });
+    app.projectPageGitHub.forEach((item) => {
+      item.classList.toggle("light-mode-svg",isLight);
+    });
+
+    app.projectPageBack.forEach((item) => {
+      item.classList.toggle("light-mode-svg",!isLight);
     });
   },
 };
