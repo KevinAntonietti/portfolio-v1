c2765ec0708343c3fa40ef387b5d980df8a15ed6
KevinAntonietti <108470673+KevinAntonietti@users.noreply.github.com>
2023-01-09 10:59:35 +0100

    package v3

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const helmet = require('helmet');
+// const helmet = require('helmet');
 
 const router = require('./app/routers/router');
 
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(helmet());
+// app.use(helmet());
 require('dotenv').config()
 
 
